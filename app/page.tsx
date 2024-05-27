b52b39ad74d2bc9dad14b16a6977ced61c2c8478
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-05-27 10:16:49 -0600

    feat: add cal embed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Main } from "@/components/craft";
 import Hero from "@/components/layout/hero";
+import Calendar from "@/components/cal";
 
 export default function Home() {
   return (
     <Main>
       <Hero />
+      <Calendar event="30min" />
     </Main>
   );
 }
