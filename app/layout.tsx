83c8a3b20a9bd2ee33b81a21b16896f03229dc88
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-09-10 12:41:03 -0700

    feat: add fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
@@ -32,6 +33,28 @@ export default function RootLayout({
         <Nav />
         {children}
         <Footer />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '824852226112291');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=824852226112291&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
